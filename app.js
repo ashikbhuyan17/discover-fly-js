76b46961c89f622eb17b30cfdd04b86b1ad94a94
ashikbhuyan17 <ashikbhuyan.swe.diu@gmail.com>
2021-01-30 20:27:40 +0600

    modified app.js,index,style.css

--- app.js
+++ app.js
@@ -1,41 +1,4 @@
 
-
-// document.getElementById('increase').addEventListener('click', function () {
-//     // let inputField = Number(document.getElementById('input').value)
-//     // inputField++;
-//     // document.getElementById('input').value = inputField
-//     // let ticketAmount = inputField * 150
-//     // console.log('increase : ' + ticketAmount);
-//     firstClassTicket(true);
-
-// })
-
-// document.getElementById('decrease').addEventListener('click', function () {
-//     // let inputField = Number(document.getElementById('input').value)
-//     // inputField--;
-//     // if (inputField < 0) {
-//     //     inputField = 0
-//     // }
-//     // document.getElementById('input').value = inputField
-//     // let ticketAmount = inputField * 150
-//     // console.log('decrease :' + ticketAmount);
-//     firstClassTicket(false);
-
-// })
-
-// function firstClassTicket(isIncrease) {
-//     let inputField = Number(document.getElementById('firstClass-count').value)
-//     if (isIncrease == true) {
-//         inputField++;
-//     }
-//     else if (isIncrease == false & inputField > 0) {
-//         inputField--;
-//     }
-//     document.getElementById('firstClass-count').value = inputField
-//     let ticketAmount = inputField * 150
-//     console.log('increase : ' + ticketAmount);
-// }
-
 function flightsBooking(ticket, isIncrease) {
     let inputField = Number(document.getElementById(ticket + '-count').value)
     if (isIncrease == true) {
@@ -52,66 +15,35 @@ function flightsBooking(ticket, isIncrease) {
     if (ticket == 'economy') {
         ticketAmount = inputField * 100
     }
-
-    console.log('increase : ' + ticketAmount);
-
+    // console.log('increase : ' + ticketAmount);
     calculateTotal();
 }
 
 
-
 function calculateTotal() {
+
+    // Subtotal
     let economyInput = Number(document.getElementById('economy-count').value)
     let firstClassInput = Number(document.getElementById('firstClass-count').value)
     const totalAmount = firstClassInput * 150 + economyInput * 100
     document.getElementById('subtotal').innerText = '$' + totalAmount
 
-    const tax = totalAmount * 0.1
+    // Charge 10% VAT
+    const tax = Math.round(totalAmount * 0.1)
     document.getElementById('total-tax').innerText = '$' + tax
 
     // grandTotal
     const grandTotal = tax + totalAmount
+    // console.log({ grandTotal });
     document.getElementById('grand-total').innerText = '$' + grandTotal
 
 }
 
-// function economyTicket(isIncrease) {
-//     let inputField = Number(document.getElementById('economy-count').value)
-//     if (isIncrease == true) {
-//         inputField++;
-//     }
-//     else if (isIncrease == false & inputField > 0) {
-//         inputField--;
-//     }
-//     document.getElementById('economy-count').value = inputField
-//     let ticketAmount = inputField * 100
-//     console.log('economyIncrease : ' + ticketAmount);
-// }
 
+document.getElementById('show').addEventListener('click', function () {
+    document.getElementById('showing').style.display = "block";
+})
 
-
-// document.getElementById('economyIncrease').addEventListener('click', function () {
-//     // let inputField = Number(document.getElementById('economyInput').value)
-//     // inputField++;
-//     // document.getElementById('economyInput').value = inputField
-//     // let ticketAmount = inputField * 100
-//     // console.log('economyIncrease : ' + ticketAmount);
-
-//     economyTicket(true);
-
-// })
-
-// document.getElementById('economyDecrease').addEventListener('click', function () {
-//     // let inputField = Number(document.getElementById('economyInput').value)
-//     // inputField--;
-//     // if (inputField < 0) {
-//     //     inputField = 0
-//     // }
-//     // document.getElementById('economyInput').value = inputField
-//     // let ticketAmount = inputField * 100
-//     // console.log('economyDecrease :' + ticketAmount);
-//     economyTicket(false);
-
-// })
-
-
+document.getElementById('closeButton').addEventListener('click', function () {
+    document.getElementById('showing').style.display = "none";
+})
